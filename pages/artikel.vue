97cddfceb96c53f9f88b762ab8fb2a7296b95b29
enkudavi <ekopur05@gmail.com>
2022-01-23 15:29:17 +0700

    add view artikel dan tutorial

---
+++ pages/artikel.vue
@@ -0,0 +1,303 @@
+<template>
+  <div class="artikel">
+    <Navbar/>  
+
+    <!--================ Start Banner Area =================-->
+    <section class="banner_area">
+        <div class="banner_inner d-flex align-items-center">
+            <div class="container mt-5 text-left">
+                <div class="row">
+                    <div class="col-sm-6 col-12">
+                        <div class="banner_content">
+                            <h2>Baca artikel lebih banyak tentang teknologi disini</h2>
+                            <p class="justify-content-center" style="color: #cfdee3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint temporibus blanditiis reprehenderit repudiandae accusamus perspiciatis!</p>
+                            <div class="page_link">
+                                <a class="btn btn-custome btn-lg" href="index.html">Baca Artikel</a>
+                                <a href="blog.html" class="btn btn-custome-second btn-lg">Subscribe</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-12 text-right mt-5 img-right">
+                        <img src="../assets/img/banner/coding-concept.png" style="width: 80%" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================ End Banner Area =================-->
+
+    <section class="blog_area mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="main_title text-left">
+                        <h2>Artikel ini bisa <br>
+                            bikin kamu pinter..</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="blog_left_sidebar">
+                        <article class="row blog_item">
+                            <div class="col-md-6">
+                                <div class="blog_post">
+                                    <img src="../assets/img/blog/main-blog/m-blog-1.jpg" alt="">
+                                    <div class="blog_details">
+                                        <a href="single-blog.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                    </div>
+                                    <div class="blog_info text-left">
+                                        <div class="post_tag blog_meta">
+                                            <a href="#"><i class="lnr lnr-user"></i> &nbsp;Mark wiens</a> - &nbsp;
+                                            <a href="#" class="text-right ml-auto"><i class="lnr lnr-calendar-full"></i> &nbsp;12 Dec, 2017</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="blog_post">
+                                    <img src="../assets/img/blog/main-blog/m-blog-2.jpg" alt="">
+                                    <div class="blog_details">
+                                        <a href="single-blog.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                    </div>
+                                    <div class="blog_info text-left">
+                                        <div class="post_tag blog_meta">
+                                            <a href="#"><i class="lnr lnr-user"></i> &nbsp;Mark wiens</a> - &nbsp;
+                                            <a href="#" class="text-right ml-auto"><i class="lnr lnr-calendar-full"></i> &nbsp;12 Dec, 2017</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="blog_post">
+                                    <img src="../assets/img/blog/main-blog/m-blog-3.jpg" alt="">
+                                    <div class="blog_details">
+                                        <a href="single-blog.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                    </div>
+                                    <div class="blog_info text-left">
+                                        <div class="post_tag blog_meta">
+                                            <a href="#"><i class="lnr lnr-user"></i> &nbsp;Mark wiens</a> - &nbsp;
+                                            <a href="#" class="text-right ml-auto"><i class="lnr lnr-calendar-full"></i> &nbsp;12 Dec, 2017</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="blog_post">
+                                    <img src="../assets/img/blog/main-blog/m-blog-4.jpg" alt="">
+                                    <div class="blog_details">
+                                        <a href="single-blog.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                    </div>
+                                    <div class="blog_info text-left">
+                                        <div class="post_tag blog_meta">
+                                            <a href="#"><i class="lnr lnr-user"></i> &nbsp;Mark wiens</a> - &nbsp;
+                                            <a href="#" class="text-right ml-auto"><i class="lnr lnr-calendar-full"></i> &nbsp;12 Dec, 2017</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="blog_post">
+                                    <img src="../assets/img/blog/main-blog/m-blog-1.jpg" alt="">
+                                    <div class="blog_details">
+                                        <a href="single-blog.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                    </div>
+                                    <div class="blog_info text-left">
+                                        <div class="post_tag blog_meta">
+                                            <a href="#"><i class="lnr lnr-user"></i> &nbsp;Mark wiens</a> - &nbsp;
+                                            <a href="#" class="text-right ml-auto"><i class="lnr lnr-calendar-full"></i> &nbsp;12 Dec, 2017</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="blog_post">
+                                    <img src="../assets/img/blog/main-blog/m-blog-5.jpg" alt="">
+                                    <div class="blog_details">
+                                        <a href="single-blog.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                    </div>
+                                    <div class="blog_info text-left">
+                                        <div class="post_tag blog_meta">
+                                            <a href="#"><i class="lnr lnr-user"></i> &nbsp;Mark wiens</a> - &nbsp;
+                                            <a href="#" class="text-right ml-auto"><i class="lnr lnr-calendar-full"></i> &nbsp;12 Dec, 2017</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                        <nav class="blog-pagination justify-content-left d-flex">
+                            <ul class="pagination">
+                                <li class="page-item">
+                                    <a href="#" class="page-link" aria-label="Previous">
+                                        <span aria-hidden="true">
+                                            <span class="lnr lnr-chevron-left"></span>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="page-item"><a href="#" class="page-link">01</a></li>
+                                <li class="page-item active"><a href="#" class="page-link">02</a></li>
+                                <li class="page-item"><a href="#" class="page-link">03</a></li>
+                                <li class="page-item"><a href="#" class="page-link">04</a></li>
+                                <li class="page-item"><a href="#" class="page-link">09</a></li>
+                                <li class="page-item">
+                                    <a href="#" class="page-link" aria-label="Next">
+                                        <span aria-hidden="true">
+                                            <span class="lnr lnr-chevron-right"></span>
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="blog_right_sidebar">
+                        <aside class="single_sidebar_widget search_widget">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Search Posts">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button"><i class="lnr lnr-magnifier"></i></button>
+                                </span>
+                            </div><!-- /input-group -->
+                            <div class="br"></div>
+                        </aside>
+                        <aside class="single_sidebar_widget author_widget">
+                            <img class="author_img rounded-circle" src="../assets/img/blog/author.png" alt="">
+                            <h4>Eko Purnomo</h4>
+                            <p>Software Engineer</p>
+                            <div class="social_icon">
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-github"></i></a>
+                                <a href="#"><i class="fa fa-behance"></i></a>
+                            </div>
+                            <p class="text-justify">Boot camps have its supporters andit sdetractors. Some people do not understand why you should have to spend money on boot camp when you can get. Boot camps have itssuppor ters andits detractors.</p>
+                            <div class="br"></div>
+                        </aside>
+                        <aside class="single_sidebar_widget popular_post_widget">
+                            <h3 class="widget_title">Popular Posts</h3>
+                            <div class="media post_item">
+                                <img src="../assets/img/blog/popular-post/post1.jpg" alt="post">
+                                <div class="media-body">
+                                    <a href="single-blog.html"><h3>Space The Final Frontier</h3></a>
+                                    <p>02 Hours ago</p>
+                                </div>
+                            </div>
+                            <div class="media post_item">
+                                <img src="../assets/img/blog/popular-post/post2.jpg" alt="post">
+                                <div class="media-body">
+                                    <a href="single-blog.html"><h3>The Amazing Hubble</h3></a>
+                                    <p>02 Hours ago</p>
+                                </div>
+                            </div>
+                            <div class="media post_item">
+                                <img src="../assets/img/blog/popular-post/post3.jpg" alt="post">
+                                <div class="media-body">
+                                    <a href="single-blog.html"><h3>Astronomy Or Astrology</h3></a>
+                                    <p>03 Hours ago</p>
+                                </div>
+                            </div>
+                            <div class="media post_item">
+                                <img src="../assets/img/blog/popular-post/post4.jpg" alt="post">
+                                <div class="media-body">
+                                    <a href="single-blog.html"><h3>Asteroids telescope</h3></a>
+                                    <p>01 Hours ago</p>
+                                </div>
+                            </div>
+                            <div class="br"></div>
+                        </aside>
+                        <aside class="single_sidebar_widget ads_widget">
+                            <a href="#"><img class="img-fluid" src="img/blog/add.jpg" alt=""></a>
+                            <div class="br"></div>
+                        </aside>
+                        <aside class="single_sidebar_widget post_category_widget">
+                            <h4 class="widget_title">Post Catgories</h4>
+                            <ul class="list cat-list">
+                                <li>
+                                    <a href="#" class="d-flex justify-content-between">
+                                        <p>Technology</p>
+                                        <p>37</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex justify-content-between">
+                                        <p>Lifestyle</p>
+                                        <p>24</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex justify-content-between">
+                                        <p>Fashion</p>
+                                        <p>59</p>
+                                    </a>
+                                </li>												
+                            </ul>
+                            <div class="br"></div>
+                        </aside>
+                        <aside class="single-sidebar-widget newsletter_widget">
+                            <h4 class="widget_title">Newsletter</h4>
+                            <p>
+                            Here, I focus on a range of items and features that we use in life without
+                            giving them a second thought.
+                            </p>
+                            <div class="form-group d-flex flex-row">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text"><i class="fa fa-envelope" aria-hidden="true"></i></div>
+                                    </div>
+                                    <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Enter email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email'">
+                                </div>
+                                <a href="#" class="bbtns">Subcribe</a>
+                            </div>	
+                            <p class="text-bottom">You can unsubscribe at any time</p>	
+                            <div class="br"></div>							
+                        </aside>
+                        <aside class="single-sidebar-widget tag_cloud_widget">
+                            <h4 class="widget_title">Tag Clouds</h4>
+                            <ul class="list">
+                                <li><a href="#">Technology</a></li>
+                                <li><a href="#">Fashion</a></li>
+                                <li><a href="#">Architecture</a></li>
+                                <li><a href="#">Fashion</a></li>
+                                <li><a href="#">Food</a></li>
+                                <li><a href="#">Technology</a></li>
+                                <li><a href="#">Lifestyle</a></li>
+                                <li><a href="#">Art</a></li>
+                                <li><a href="#">Adventure</a></li>
+                                <li><a href="#">Food</a></li>
+                                <li><a href="#">Lifestyle</a></li>
+                                <li><a href="#">Adventure</a></li>
+                            </ul>
+                        </aside>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <Footer/>
+  </div>
+</template>
+
+<script>
+
+import Navbar from '../components/Navbar.vue'
+import Footer from '../components/Footer.vue'
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
+
+
+<style scoped>
+  .btn-custome{
+    background-color: blue;
+  }
+
+  .btn-custome-second{
+    background-color: grey;
+  }
+</style>
---
+++ pages/tutorial.vue
@@ -0,0 +1,179 @@
+<template>
+    <div>
+        <Navbar/>
+        <section class="banner_area">
+            <div class="banner_inner d-flex align-items-center">
+                <div class="container mt-5 text-left">
+                    <div class="row">
+                        <div class="col-sm-6 col-12">
+                            <div class="banner_content">
+                                <h2>Design, Coding, Cyber Security, Semua ada disini </h2>
+                                <p class="justify-content-center" style="color: #cfdee3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint temporibus blanditiis reprehenderit repudiandae accusamus perspiciatis!</p>
+                                <div class="page_link">
+                                    <a class="btn btn-custome btn-lg" href="index.html">Baca Artikel</a>
+                                    <a href="blog.html" class="btn btn-custome-second btn-lg">Subscribe</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-12 text-right mt-5 img-right">
+                            <img src="../assets/img/banner/coding-concept.png" style="width: 80%" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="blog_area mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="main_title text-left">
+                        <h2>Mulai pelajari  <br>
+                            apa yang kamu suka</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="blog_left_sidebar">
+                        <article class="row blog_item">
+                            <div class="col-md-4">
+                                <div class="blog_post">
+                                    <img src="../assets/img/blog/main-blog/m-blog-1.jpg" class="rounded" alt="">
+                                    <div class="blog_details">
+                                        <a href="single-blog.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                    </div>
+                                    <div class="blog_info text-left">
+                                        <div class="post_tag blog_meta">
+                                            <a href="#"><i class="fa fa-youtube"></i> &nbsp;Mark wiens</a> - &nbsp;
+                                            <a href="#" class="text-right ml-auto"><i class="fa fa-play"></i> &nbsp;20 Video</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="blog_post">
+                                    <img src="../assets/img/blog/main-blog/m-blog-2.jpg" alt="" class="rounded">
+                                    <div class="blog_details">
+                                        <a href="single-blog.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                    </div>
+                                    <div class="blog_info text-left">
+                                        <div class="post_tag blog_meta">
+                                            <a href="#"><i class="fa fa-youtube"></i> &nbsp;Mark wiens</a> - &nbsp;
+                                            <a href="#" class="text-right ml-auto"><i class="fa fa-play"></i> &nbsp;20 Video</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="blog_post">
+                                    <img src="../assets/img/blog/main-blog/m-blog-3.jpg" alt="" class="rounded">
+                                    <div class="blog_details">
+                                        <a href="single-blog.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                    </div>
+                                    <div class="blog_info text-left">
+                                        <div class="post_tag blog_meta">
+                                            <a href="#"><i class="fa fa-youtube"></i> &nbsp;Mark wiens</a> - &nbsp;
+                                            <a href="#" class="text-right ml-auto"><i class="fa fa-play"></i> &nbsp;20 Video</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="blog_post">
+                                    <img src="../assets/img/blog/main-blog/m-blog-4.jpg" alt="" class="rounded">
+                                    <div class="blog_details">
+                                        <a href="single-blog.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                    </div>
+                                    <div class="blog_info text-left">
+                                        <div class="post_tag blog_meta">
+                                            <a href="#"><i class="fa fa-youtube"></i> &nbsp;Mark wiens</a> - &nbsp;
+                                            <a href="#" class="text-right ml-auto"><i class="fa fa-play"></i> &nbsp;20 Video</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="blog_post">
+                                    <img src="../assets/img/blog/main-blog/m-blog-1.jpg" alt="" class="rounded">
+                                    <div class="blog_details">
+                                        <a href="single-blog.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                    </div>
+                                    <div class="blog_info text-left">
+                                        <div class="post_tag blog_meta">
+                                            <a href="#"><i class="fa fa-youtube"></i> &nbsp;Mark wiens</a> - &nbsp;
+                                            <a href="#" class="text-right ml-auto"><i class="fa fa-play"></i> &nbsp;20 Video</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="blog_post">
+                                    <img src="../assets/img/blog/main-blog/m-blog-5.jpg" alt="" class="rounded">
+                                    <div class="blog_details">
+                                        <a href="single-blog.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                    </div>
+                                    <div class="blog_info text-left">
+                                        <div class="post_tag blog_meta">
+                                            <a href="#"><i class="fa fa-youtube"></i> &nbsp;Mark wiens</a> - &nbsp;
+                                            <a href="#" class="text-right ml-auto"><i class="fa fa-play"></i> &nbsp;20 Video</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                        <nav class="blog-pagination justify-content-left d-flex">
+                            <ul class="pagination">
+                                <li class="page-item">
+                                    <a href="#" class="page-link" aria-label="Previous">
+                                        <span aria-hidden="true">
+                                            <span class="lnr lnr-chevron-left"></span>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="page-item"><a href="#" class="page-link">01</a></li>
+                                <li class="page-item active"><a href="#" class="page-link">02</a></li>
+                                <li class="page-item"><a href="#" class="page-link">03</a></li>
+                                <li class="page-item"><a href="#" class="page-link">04</a></li>
+                                <li class="page-item"><a href="#" class="page-link">09</a></li>
+                                <li class="page-item">
+                                    <a href="#" class="page-link" aria-label="Next">
+                                        <span aria-hidden="true">
+                                            <span class="lnr lnr-chevron-right"></span>
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <Footer/>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+    name: 'Tutorial',
+    components: {
+        Navbar,
+        Footer
+    }
+}
+</script>
+
+<style scoped>
+  .btn-custome{
+    background-color: blue;
+  }
+
+  .btn-custome-second{
+    background-color: grey;
+  }
+</style>
